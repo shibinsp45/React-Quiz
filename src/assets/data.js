a503c09176eb27b3a79bc5e8472e4011124e9f93
Shibin S P <shibinsp45@gmail.com>
2023-12-23 12:11:19 +0530

    new files done

---
+++ src/assets/data.js
@@ -0,0 +1,82 @@
+export const data = [
+  {
+    question: "What does HTML stand for?",
+    option1: "Hyperlink and Text Markup Language",
+    option2: "Hyper Text Markup Language",
+    option3: "High-level Text Markup Language",
+    option4: "Hyper Transfer Markup Language",
+    ans: 2, 
+  },
+  {
+    question: "What does CSS stand for?",
+    option1: "Counter Style Sheet",
+    option2: "Computer Style Sheet",
+    option3: "Cascading Style Sheet",
+    option4: "Creative Style Sheet",
+    ans: 3, 
+  },
+  {
+    question: "Which HTML tag is used to create a hyperlink?",
+    option1: "<link>",
+    option2: "<a>",
+    option3: "<href>",
+    option4: "<hyperlink>",
+    ans: 2, 
+  },
+  {
+    question: "What is the purpose of the CSS property 'margin'?",
+    option1: "To add space inside an element",
+    option2: "To add space outside an element",
+    option3: "To change the text color",
+    option4: "To change the font size",
+    ans: 2, 
+  },
+  {
+    question: "Which of the following is not a valid color in CSS?",
+    option1: "Red",
+    option2: "Green",
+    option3: "Blue",
+    option4: "Transparent",
+    ans: 4, // Correct answer is represented by the option number
+  },
+  {
+    question: "What does the 'box model' in CSS consist of?",
+    option1: "Content, Padding, Border, Margin",
+    option2: "Header, Footer, Sidebar, Main",
+    option3: "HTML, CSS, JavaScript",
+    option4: "Array, Object, String, Number",
+    ans: 1, // Correct answer is represented by the option number
+  },
+  {
+    question: "Which HTML tag is used to define an unordered list?",
+    option1: "<ul>",
+    option2: "<ol>",
+    option3: "<li>",
+    option4: "<list>",
+    ans: 1, // Correct answer is represented by the option number
+  },
+  {
+    question: "In JavaScript, what does the DOM stand for?",
+    option1: "Document Object Model",
+    option2: "Data Object Model",
+    option3: "Design Object Model",
+    option4: "Document Oriented Model",
+    ans: 1, // Correct answer is represented by the option number
+  },
+  {
+    question: "What is the purpose of the 'alt' attribute in an HTML image tag?",
+    option1: "To specify the image source",
+    option2: "To provide alternative text for the image",
+    option3: "To set the image width and height",
+    option4: "To define the image alignment",
+    ans: 2, // Correct answer is represented by the option number
+  },
+  {
+    question: "Which CSS property is used to change the background color of an element?",
+    option1: "background-color",
+    option2: "color",
+    option3: "text-color",
+    option4: "bgcolor",
+    ans: 1, // Correct answer is represented by the option number
+  },
+];
